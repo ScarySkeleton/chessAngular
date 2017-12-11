e7a6283ea052061fe83b9e74b54da3dae7fa5fd9
Danylo Bilokha <Danylo_Bilokha@epam.com>
2017-12-11 12:48:28 +0200

    change links.js to .ts extenssion, change structure, change def angular style extenssion from .css to .scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import LogoComponent from '../../../shared/components/logo/logo.component';
-import NavComponent from '../../../shared/components/nav/nav.component';
-import TabComponent from '../../../shared/components/tab/tab.component';
+import LogoComponent from '../shared/components/logo/logo.component';
+import NavComponent from '../shared/components/nav/nav.component';
+import TabComponent from '../shared/components/tab/tab.component';
 
 @NgModule({
   declarations: [
