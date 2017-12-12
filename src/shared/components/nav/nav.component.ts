bbe68c0cb77413fc46646afa30e3442399f73013
Danylo Bilokha <Danylo_Bilokha@epam.com>
2017-12-12 17:30:26 +0200

    fix the styles

--- src/shared/components/nav/nav.component.ts
+++ src/shared/components/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 import ILink from '../../../assets/pages/ILink';
 import links from '../../../assets/pages/links.js';
@@ -7,7 +7,8 @@ import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 @Component({
     selector: 'chess-nav',
     templateUrl: './nav.component.html',
-    styleUrls: ['./nav.component.scss']
+    styleUrls: ['./nav.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 export default class NavComponent {
     links: Array<ILink> = links
