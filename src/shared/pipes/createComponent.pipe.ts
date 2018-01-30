bb3821e62003cf128e53aca4ff12366cb8ee2bc1
Danylo Bilokha <belohadk97@gmail.com>
2018-01-30 16:31:45 +0200

    case when the no anchor - ONLY text: Can retrieve innerHTML, but it dont resolve but Angular and inserted in DOM.

--- src/shared/pipes/createComponent.pipe.ts
+++ src/shared/pipes/createComponent.pipe.ts
@@ -1,30 +1,39 @@
-import {Pipe, ViewContainerRef, ViewChild, Inject, Injector, ViewRef, ApplicationRef} from '@angular/core';
+import {Pipe, ViewContainerRef, ViewChild, Inject, Injector, ViewRef, ApplicationRef, PipeTransform} from '@angular/core';
 import {ComponentFactoryResolver} from '@angular/core';
 import {TemplateRef} from '@angular/core/src/linker/template_ref';
 
 @Pipe({
     name: 'createComponent'
 })
-class CreateComponentPipe {
+class CreateComponentPipe implements PipeTransform {
 
     private componentFactoryResolver: any;
 
     constructor(@Inject(ComponentFactoryResolver) factoryResolver,
         private viewContainerRef: ViewContainerRef,
-        private injector: Injector) {
+        private injector: Injector,
+        private app: ApplicationRef) {
             this.componentFactoryResolver = factoryResolver;
     }
 
-    transform(component: any, container: ViewContainerRef): void {
+    transform(component: Function, container: any): any {
 
         if(!component)
             return;
 
-        console.log(component, container);
-
         const _factory = this.componentFactoryResolver.resolveComponentFactory(component);
-        const _ref = _factory.create(this.injector, [], container);
-        //this.app.attachView(_ref.hostView);
+        
+        let _container;
+        // *(1) For case when we don't pass a place to insert
+        if(!container) {
+            _container = document.createElement('div');
+        }
+
+        const _ref = _factory.create(this.injector, [], container || _container);
+        // *(1)
+        if(!container)
+            return _ref.location.nativeElement.innerText;
+             
     }
 }
 
